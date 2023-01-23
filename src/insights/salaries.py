b67343e2bd12a8f4ba504551a5e686296230d0c4
Roberto Luna <robertocgw@gmail.com>
2023-01-23 16:58:45 -0300

    feat: req4 - Implemente a função get_max_salary

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,17 @@
 from typing import Union, List, Dict
+from src.insights.jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    data_jobs = read(path)
+    max_salary = 0
+    for job in data_jobs:
+        if job["max_salary"] != "" and not job["max_salary"].isalpha():
+            salary = int(job["max_salary"])
+            if salary > max_salary:
+                max_salary = salary
+    return max_salary
+    # raise NotImplementedError
 
 
 def get_min_salary(path: str) -> int:
@@ -64,8 +59,7 @@ def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
 
 
 def filter_by_salary_range(
-    jobs: List[dict],
-    salary: Union[str, int]
+    jobs: List[dict], salary: Union[str, int]
 ) -> List[Dict]:
     """Filters a list of jobs by salary range
 
