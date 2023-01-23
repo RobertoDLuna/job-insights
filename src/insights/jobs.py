275d425898b90a172c9360e48edb8c6eeb8a212d
Roberto Luna <robertocgw@gmail.com>
2023-01-23 17:31:18 -0300

    feat: req6 - Implemente a função filter_by_job_type

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -22,18 +22,9 @@ def get_unique_job_types(path: str) -> List[str]:
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    raise NotImplementedError
+    filtered_by_job_types = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            filtered_by_job_types.append(job)
+    return filtered_by_job_types
+    # raise NotImplementedError
