99c30d06f5777a0168ec699ac97903f7d925fb5b
Roberto Luna <robertocgw@gmail.com>
2023-01-30 10:56:38 -0300

    feat: req10 - Implemente um teste para a função count_ocurrences

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,16 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = 'data/jobs.csv'
+
+    with open(path) as file:
+        data = file.read()
+
+        expected = {
+            "python": data.lower().count("python"),
+            "javascript": data.lower().count("javascript"),
+        }
+
+        assert expected["python"] == count_ocurrences(path, 'python')
+        assert expected["javascript"] == count_ocurrences(path, 'javascript')
